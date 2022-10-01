20f9a03a9a76883e6140ca3c4dcc28cba193f8dc
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-01 19:38:01 -0400

    Can see some of the users

--- client/src/App.js
+++ client/src/App.js
@@ -29,9 +29,47 @@ function App() {
 
 
   // useEffect()
-  fetch("http://localhost:3000/api").then(
-    response => console.log(response)
+  // fetch("http://localhost:3000/api").then(
+  //   response => console.log(response.body)
+  // )
+  fetch("http://localhost:3000/api")
+  .then((response) => response.body)
+  .then((rb) => {
+    const reader = rb.getReader();
+
+    return new ReadableStream({
+      start(controller) {
+        // The following function handles each data chunk
+        function push() {
+          // "done" is a Boolean and value a "Uint8Array"
+          reader.read().then(({ done, value }) => {
+            // If there is no more data to read
+            if (done) {
+              console.log('done', done);
+              controller.close();
+              return;
+            }
+            // Get the data and send it to the browser via the controller
+            controller.enqueue(value);
+            // Check chunks by logging to the console
+            console.log(done, value);
+            push();
+          });
+        }
+
+        push();
+      },
+    });
+  })
+  .then((stream) =>
+    // Respond with our stream
+    new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text()
   )
+  .then((result) => {
+    // Do things with result
+    console.log(result);
+  });
+
 
 
 
