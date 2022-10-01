d5d5e98576a496f2e908bf8afadb1d70f916fcd8
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-01 13:36:49 -0400

    changed listening port for sever

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ const { application } = require("express")
 const express = require("express")
 const app = express()
 
-app.get("api/", (req, res) =>  {
+app.get("/api", (req, res) =>  {
     res.json({"users" : ['userOne', 'userTwo', 'userThree']})
 })
 
-app.listen(5000, () => { console.log("Server started on 5000")})
+app.listen(3000, () => { console.log("Server started on 3000")})
