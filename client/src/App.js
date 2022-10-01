2fe6187aa553b3caa374dd8a6e360ac9ab3974a6
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-01 15:31:54 -0400

    tried to fix backend :(

--- client/src/App.js
+++ client/src/App.js
@@ -17,18 +17,40 @@ function App() {
   const [backendData, setBackendData] = useState([{}])
 
   useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
+    fetch("http://localhost:3000/api").then(
+    response => response.json
     ).then(
       data => {
         setBackendData(data)
+        console.log('tried to fetch')
       }
     )
   }, [])
 
-  /*
-To display data on frontend:
-<div>
+
+  // useEffect()
+  // fetch("http://localhost:3000/api").then(
+  //   response => console.log(response)
+  // )
+
+
+
+/* <div>
+
+{(typeof backendData.users === 'undefined') ? (
+  <p>Loading...</p>
+): (
+  backendData.user.map((user, i) => (
+    <p key={i}> {user}</p>
+  ))
+)}
+
+
+</div> */
+
+return 
+ (
+  <div>
 
 {(typeof backendData.users === 'undefined') ? (
   <p>Loading...</p>
@@ -40,30 +62,31 @@ To display data on frontend:
 
 
 </div>
+ )
 
-  */
 
 
-  return (
-    <Router>
-    <Fragment>
-      <MainNavigation/>
-      <Routes>
-        <Route exact path='/' element={<Home/>}>
-          {/* <Route exact path='/' element={<Home/>}/> */}
-        </Route>
-        <Route exact path='/search' element={<Search details={initialDetails}/>}/>
-        {/* <Route exact path='/login' element={<Login/>}/> */}
-        <Route exact path='/register' element={<Register/>}/>
-        <Route exact path='/vision' element={<Vision/>}/>
+  // return (
+    
+  //   <Router>
+  //   <Fragment>
+  //     <MainNavigation/>
+  //     <Routes>
+  //       <Route exact path='/' element={<Home/>}>
+  //         {/* <Route exact path='/' element={<Home/>}/> */}
+  //       </Route>
+  //       <Route exact path='/search' element={<Search details={initialDetails}/>}/>
+  //       {/* <Route exact path='/login' element={<Login/>}/> */}
+  //       <Route exact path='/register' element={<Register/>}/>
+  //       <Route exact path='/vision' element={<Vision/>}/>
 
-      </Routes>
-    </Fragment>
-    <BottomNavigation> 1 2 3 4</BottomNavigation>
-  </Router>
+  //     </Routes>
+  //   </Fragment>
+  //   <BottomNavigation> 1 2 3 4</BottomNavigation>
+  // </Router>
 
   
-  );
+  // );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const { application } = require("express")
 const express = require("express")
+const cors = require('cors')
 const app = express()
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        callback(null, true)
+    },
+}
+app.options('*', cors(corsOptions))
 
 app.get("/api", (req, res) =>  {
     res.json({"users" : ['userOne', 'userTwo', 'userThree']})
