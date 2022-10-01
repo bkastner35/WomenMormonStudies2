2fddd8ed99d17bdedc5eebaf557e3c1926e1a83d
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-01 12:12:08 -0400

    setup template code for frontend to see back

---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import { BottomNavigation } from "@mui/material";
+import React, {useEffect, useState, Fragment} from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import MainNavigation from "./componenet/MainNav.js";
+import Search from "./componenet/Search.js";
+import initialDetails from "./data/initialDetails.js";
+import Home from "./pages/Home";
+import Register from "./pages/Register.js";
+import Vision from "./pages/Vision.js";
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  /*
+To display data on frontend:
+<div>
+
+{(typeof backendData.users === 'undefined') ? (
+  <p>Loading...</p>
+): (
+  backendData.user.map((user, i) => (
+    <p key={i}> {user}</p>
+  ))
+)}
+
+
+</div>
+
+  */
+
+
+  return (
+    <Router>
+    <Fragment>
+      <MainNavigation/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}>
+          {/* <Route exact path='/' element={<Home/>}/> */}
+        </Route>
+        <Route exact path='/search' element={<Search details={initialDetails}/>}/>
+        {/* <Route exact path='/login' element={<Login/>}/> */}
+        <Route exact path='/register' element={<Register/>}/>
+        <Route exact path='/vision' element={<Vision/>}/>
+
+      </Routes>
+    </Fragment>
+    <BottomNavigation> 1 2 3 4</BottomNavigation>
+  </Router>
+
+  
+  );
+}
+
+export default App;
