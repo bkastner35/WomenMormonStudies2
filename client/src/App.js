a15f84e5b88e693d17f3a489662bccc6366684dd
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-03 20:37:23 -0400

    Got communication with backend working

--- client/src/App.js
+++ client/src/App.js
@@ -15,119 +15,75 @@ import Vision from "./pages/Vision.js";
 
 function App() {
 
-  async function getDataPromise() {
-    // Want to wait for fetch request
-  const response = await fetch("http://localhost:3000/api")
-  async function getStreamFromBody(body) {
-    const reader = await body.getReader();
+  const [backend, setBackendData] = useState([{}])
 
-    return new ReadableStream({
-      start(controller) {
-        // The following function handles each data chunk
-        async function push() {
-          // "done" is a Boolean and value a "Uint8Array"
-          reader.read().then(({ done, value }) => {
-            // If there is no more data to read
-            if (done) {
-              controller.close();
-              return;
-            }
-            // Get the data and send it to the browser via the controller
-            controller.enqueue(value);
-            push();
-          });
-        }
-
-        push();
-      },
-    });
-  }
-    // Ready in request as a strea
-    const stream = await (response.body);
-    // Translate stream -> string -> map
-    const result = await new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text()
-    function getProfiles() {
+  useEffect (() => {
+    fetch("http://localhost:3000/api").then((response) => {
+      const body = response.body
+      const reader = body.getReader();
+      return new ReadableStream({
+        start(controller) {
+          // The following function handles each data chunk
+          async function push() {
+            // "done" is a Boolean and value a "Uint8Array"
+            reader.read().then(({ done, value }) => {
+              // If there is no more data to read
+              if (done) {
+                controller.close();
+                return;
+              }
+              // Get the data and send it to the browser via the controller
+              controller.enqueue(value);
+              push();
+            });
+          }
+  
+          push();
+        },
+      })
+    }).then((stream) => {
+      return new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text()
+    }).then((result) => {
       const dataMap = JSON.parse(result);
-      // var profileDetails = dataMap["users"]
-      // return profileDetails
-      // Try to return as array instead
-      return Object.values(dataMap);
-    }
-    return getProfiles()
-  }
-  // Get Map from promise
-  function getValueFromPromise() {
-    const dataPromise = Promise.resolve(getDataPromise())
-    return dataPromise.then((data) => {
-      return data[0]
-    })
-  }
-  const valuesPromise = getValueFromPromise();
-  valuesPromise.then((data) => {
-    console.log(data)
-        return (
-      <Router>
-      <Fragment>
-        <MainNavigation/>
-        <Routes>
-          <Route exact path='/' element={<Home/>}>
-            {/* <Route exact path='/' element={<Home/>}/> */}
-          </Route>
-          <Route exact path='/search' element={<Search details={data}/>}/>
-          {/* <Route exact path='/login' element={<Login/>}/> */}
-          <Route exact path='/register' element={<Register/>}/>
-          <Route exact path='/vision' element={<Vision/>}/>
-  
-        </Routes>
-      </Fragment>
-      <BottomNavigation> 1 2 3 4</BottomNavigation>
-    </Router>
-    );
+      return dataMap
+    }).then(
+      data => {
+        console.log(data)
+        setBackendData(data)
+      }
+    )
+  }, [])
 
-  })
+  console.log(backend.users)
+  return (
 
-  
+    <div>
 
+      {(typeof backend.users === 'undefined') ?
+      (
+        <p>Loading...</p>
+      ):
+      (
+        <Router>
+        <Fragment>
+          <MainNavigation/>
+          <Routes>
+            <Route exact path='/' element={<Home/>}>
+              {/* <Route exact path='/' element={<Home/>}/> */}
+            </Route>
+            <Route exact path='/search' element={<Search details={backend.users}/>}/>
+            {/* <Route exact path='/login' element={<Login/>}/> */}
+            <Route exact path='/register' element={<Register/>}/>
+            <Route exact path='/vision' element={<Vision/>}/>
+    
+          </Routes>
+        </Fragment>
+        <BottomNavigation> 1 2 3 4</BottomNavigation>
+      </Router>
+      )}
+    </div>
+  )
 
-
-
-  // console.log('ProfileData:')
-  // console.log(profileData[0]) // write a non retarded way of getting this
-  // console.log(typeof(profileData))
-  // console.log('InitialDetails')
-  // console.log(initialDetails)
-  // console.log(typeof(initialDetails))
-
-  // Waitiing to fetch profile data
-  // if (typeof(profileDetails) == 'undefined' || typeof(profileDetails) == 'object Promise' || typeof(profileDetails) == 'Promise') {
-  //   return (
-
-  //     <div>
-  //       <p>Loading...</p>
-  //     </div>
-  //   )
-  // }
-  // Content had loaded
-  // else {
-  //   return (
-  //     <Router>
-  //     <Fragment>
-  //       <MainNavigation/>
-  //       <Routes>
-  //         <Route exact path='/' element={<Home/>}>
-  //           {/* <Route exact path='/' element={<Home/>}/> */}
-  //         </Route>
-  //         <Route exact path='/search' element={<Search details={initialDetails}/>}/>
-  //         {/* <Route exact path='/login' element={<Login/>}/> */}
-  //         <Route exact path='/register' element={<Register/>}/>
-  //         <Route exact path='/vision' element={<Vision/>}/>
-  
-  //       </Routes>
-  //     </Fragment>
-  //     <BottomNavigation> 1 2 3 4</BottomNavigation>
-  //   </Router>
-  //   );
-  // }
 
 }
 
