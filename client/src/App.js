adabfdd3b5431e2c791b2fcfc5fa153aae0fa9a2
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-03 20:02:32 -0400

    Tried with promises

--- client/src/App.js
+++ client/src/App.js
@@ -13,12 +13,13 @@ import Register from "./pages/Register.js";
 import Vision from "./pages/Vision.js";
 
 
-async function App() {
+function App() {
 
-  // Want to wait for fetch request
+  async function getDataPromise() {
+    // Want to wait for fetch request
   const response = await fetch("http://localhost:3000/api")
   async function getStreamFromBody(body) {
-    const reader = body.getReader();
+    const reader = await body.getReader();
 
     return new ReadableStream({
       start(controller) {
@@ -41,39 +42,30 @@ async function App() {
       },
     });
   }
-  // Ready in request as a strea
-  const stream = await (response.body);
-  // Translate stream -> string -> map
-  const result = await new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text()
-  async function getProfiles() {
-    const dataMap = JSON.parse(result);
-    // var profileDetails = dataMap["users"]
-    // return profileDetails
-    // Try to return as array instead
-    return Object.values(dataMap);
+    // Ready in request as a strea
+    const stream = await (response.body);
+    // Translate stream -> string -> map
+    const result = await new Response(stream, { headers: { 'Content-Type': 'text/html' } }).text()
+    function getProfiles() {
+      const dataMap = JSON.parse(result);
+      // var profileDetails = dataMap["users"]
+      // return profileDetails
+      // Try to return as array instead
+      return Object.values(dataMap);
+    }
+    return getProfiles()
   }
-  // Profile data hold array of profile JSON objects
-  let profileData = await getProfiles();
-
-  console.log('ProfileData:')
-  console.log(profileData)
-  console.log(typeof(profileData))
-  console.log('InitialDetails')
-  console.log(initialDetails)
-  console.log(typeof(initialDetails))
-
-  // Waitiing to fetch profile data
-  if (typeof(profileDetails) == 'undefined' || typeof(profileDetails) == 'object') {
-    return (
-
-      <div>
-        <p>Loading...</p>
-      </div>
-    )
+  // Get Map from promise
+  function getValueFromPromise() {
+    const dataPromise = Promise.resolve(getDataPromise())
+    return dataPromise.then((data) => {
+      return data[0]
+    })
   }
-  // Content had loaded
-  else {
-    return (
+  const valuesPromise = getValueFromPromise();
+  valuesPromise.then((data) => {
+    console.log(data)
+        return (
       <Router>
       <Fragment>
         <MainNavigation/>
@@ -81,7 +73,7 @@ async function App() {
           <Route exact path='/' element={<Home/>}>
             {/* <Route exact path='/' element={<Home/>}/> */}
           </Route>
-          <Route exact path='/search' element={<Search details={profileData}/>}/>
+          <Route exact path='/search' element={<Search details={data}/>}/>
           {/* <Route exact path='/login' element={<Login/>}/> */}
           <Route exact path='/register' element={<Register/>}/>
           <Route exact path='/vision' element={<Vision/>}/>
@@ -91,7 +83,51 @@ async function App() {
       <BottomNavigation> 1 2 3 4</BottomNavigation>
     </Router>
     );
-  }
+
+  })
+
+  
+
+
+
+
+  // console.log('ProfileData:')
+  // console.log(profileData[0]) // write a non retarded way of getting this
+  // console.log(typeof(profileData))
+  // console.log('InitialDetails')
+  // console.log(initialDetails)
+  // console.log(typeof(initialDetails))
+
+  // Waitiing to fetch profile data
+  // if (typeof(profileDetails) == 'undefined' || typeof(profileDetails) == 'object Promise' || typeof(profileDetails) == 'Promise') {
+  //   return (
+
+  //     <div>
+  //       <p>Loading...</p>
+  //     </div>
+  //   )
+  // }
+  // Content had loaded
+  // else {
+  //   return (
+  //     <Router>
+  //     <Fragment>
+  //       <MainNavigation/>
+  //       <Routes>
+  //         <Route exact path='/' element={<Home/>}>
+  //           {/* <Route exact path='/' element={<Home/>}/> */}
+  //         </Route>
+  //         <Route exact path='/search' element={<Search details={initialDetails}/>}/>
+  //         {/* <Route exact path='/login' element={<Login/>}/> */}
+  //         <Route exact path='/register' element={<Register/>}/>
+  //         <Route exact path='/vision' element={<Vision/>}/>
+  
+  //       </Routes>
+  //     </Fragment>
+  //     <BottomNavigation> 1 2 3 4</BottomNavigation>
+  //   </Router>
+  //   );
+  // }
 
 }
 
