412d8b59b4499529ad516c2593504ab5635aecda
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-10 16:07:34 -0400

    Need to fix connection status

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,2 +1,19 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
+
+const app = express()
+
+app.use(express.json())
+var database
+
+app.get('/', (req, resp) => {
+    resp.send('Welcome to mongodb api')
+})
+
+app.listen(8000, () => {
+    MongoClient.connect('mongodb://localhost:27017', {useNewURLParser: true}, (error, result) => {
+        if (error) throw error
+        database = result.db('Mormon')
+        console.log('Connection sucessful')
+    })
+})
