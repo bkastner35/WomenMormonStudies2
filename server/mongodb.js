53a62aaa280b5edf678b04fe573f19dcda87c572
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-10 19:51:52 -0400

    Fetches Experts data

--- server/mongodb.js
+++ server/mongodb.js
@@ -8,6 +8,14 @@ var database
 app.get('/', (req, resp) => {
     resp.send('Welcome to mongodb api')
 })
+
+// To access mormon users
+app.get('/api/Experts', (req, resp) => {
+    database.collection('Experts').find({}).toArray((err, result) => {
+        if (err) throw err
+        resp.send(result)
+    })
+})
 const uri = "mongodb+srv://ronnaksaxena:Federer132001%21@testing.0coh3qi.mongodb.net/?retryWrites=true&w=majority";
 
 app.listen(8080, () => {
