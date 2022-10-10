4afc90d55a894e63ad8d378c3867411a6502298c
ronnaksaxena <rsaxena@hamilton.edu>
2022-10-10 19:43:45 -0400

    Got connection to mongodb working

--- server/mongodb.js
+++ server/mongodb.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const MongoClient = require('mongodb').MongoClient
-
 const app = express()
 
 app.use(express.json())
@@ -9,11 +8,14 @@ var database
 app.get('/', (req, resp) => {
     resp.send('Welcome to mongodb api')
 })
+const uri = "mongodb+srv://ronnaksaxena:Federer132001%21@testing.0coh3qi.mongodb.net/?retryWrites=true&w=majority";
 
-app.listen(8000, () => {
-    MongoClient.connect('mongodb://localhost:27017', {useNewURLParser: true}, (error, result) => {
+app.listen(8080, () => {
+    MongoClient.connect( uri, {useNewURLParser: true}, (error, result) => {
         if (error) throw error
         database = result.db('Mormon')
         console.log('Connection sucessful')
     })
 })
+
+
