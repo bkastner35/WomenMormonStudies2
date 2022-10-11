3ff44b7b6ce272e59350960e7381ef59e7139616
omar <jlopezal@hamilton.edu>
2022-10-11 15:58:55 -0400

    Search Fixed

--- client/src/componenet/Card.js
+++ client/src/componenet/Card.js
@@ -19,12 +19,12 @@ import React from 'react';
   return(
     <button onClick={() => this.handleClick()}>
     <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-    <img className="br-100 h3 w3 dib" alt={this.person.name} src={process.env.PUBLIC_URL + this.person.imgPath} />
     <div> 
       
-      <h2>{this.person.name}</h2>
+      <h2>{this.person.first_name}</h2>
+      <h2>{this.person.last_name}</h2>
       <p>{this.person.email}</p>
-      <p>{this.person.interest}</p>
+      <p>{this.person.discipline}</p>
     </div>
   </div>
   </button>
