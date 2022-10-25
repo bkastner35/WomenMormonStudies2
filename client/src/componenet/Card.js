c80529360b74dce5c119d0f67397668da5de5ca8
omar <jlopezal@hamilton.edu>
2022-10-25 09:25:02 -0400

    updated find expert

--- client/src/componenet/Card.js
+++ client/src/componenet/Card.js
@@ -1,9 +1,46 @@
-// src/components/Card.js
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
-import React from 'react';
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? 'green' : 'burgundy',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
+function FormRow() {
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Item>Name</Item>
+      </Grid>
+    </React.Fragment>
+  );
+}
 
- class Card extends React.Component {
+function NestedGrid() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={1}>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+        <Grid container item spacing={3}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+class Card extends React.Component {
   constructor(props) {
   super(props);
     this.person = props.person
@@ -13,26 +50,20 @@ import React from 'react';
     window.location.href="https://www.youtube.com/watch?v=ihloYYx7qfY"
   };
 
- 
-
  render() {
   return(
-    <button onClick={() => this.handleClick()}>
-    <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
-    <img className="br-100 h3 w3 dib" alt={this.person.name} src={process.env.PUBLIC_URL + this.person.imgPath} />
-    <div> 
-      
-      <h2>{this.person.name}</h2>
-      <p>{this.person.email}</p>
-      <p>{this.person.interest}</p>
-    </div>
-  </div>
-  </button>
-  
+    <Box>
+    <Grid item xs={100}>
+    <Item>
+    <button onClick={() => this.handleClick()}>Name: {this.person.first_name} {this.person.last_name} Email: {this.person.email}</button>
+    </Item>
+    </Grid>
+    </Box>
 );
   }
 
  }
+
 
 
 export default Card;
--- client/src/componenet/Search.js
+++ client/src/componenet/Search.js
@@ -1,13 +1,56 @@
 
-
 import React, { useState } from 'react';
 import Scroll from './Scroll';
 import SearchList from './SearchList';
+import { useEffect, useRef} from "react"
+let skills = [
+  "Africa",
+  "Asia",
+  "Australia and/or New Zealand",
+  "Pacific Islands",
+  "Europe",
+  "United States and/or Canada",
+  "Latin America and/or Caribbean",
+  "Middle East"
 
+]
 
 function Search({ details }) {
 
   const [searchField, setSearchField] = useState("");
+  const [selectedSkill, setSelectedSkill] = useState("")
+  const [dropdownSearchValue, setDropdownSearchValue] = useState("")
+  const [editMode, setEditMode] = useState(false)
+  const dropdownRef = useRef()
+
+  useEffect(() => {
+    const checkIfClickedOutside = e => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (
+        editMode &&
+        dropdownRef.current &&
+        !dropdownRef.current.contains(e.target)
+      ) {
+        setEditMode(false)
+      }
+    }
+    document.addEventListener("mousedown", checkIfClickedOutside)
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside)
+    }
+  }, [editMode])
+
+  const skillSelectionHandler = skill => {
+    setSelectedSkill(skill)
+    setDropdownSearchValue("")
+    setEditMode(false)
+  }
+
+  const filteredSkills = skills.filter(skill =>
+    skill.match(new RegExp(dropdownSearchValue, "i"))
+  )
 
   const filteredPersons = details.filter(
     person => {
@@ -22,10 +65,10 @@ function Search({ details }) {
         .includes(searchField.toLowerCase())
         )
       }
-      else {
+      else{
         return (
           person
-        .last_name
+        .first_name
         .toLowerCase()
         .includes(searchField.toLowerCase())
         )
@@ -33,9 +76,9 @@ function Search({ details }) {
     }
   );
 
-  const handleChange = e => {
-    setSearchField(e.target.value);
-  };
+const handleChange = e => {
+  setSearchField(e.target.value);
+};
 
   function searchList() {
     return (
@@ -47,6 +90,47 @@ function Search({ details }) {
 
   return (
     <section className="garamond">
+          
+    <div className="App">
+      <h2>Geographic Filtering</h2>
+
+      {editMode ? (
+        // display the dropdown when the input us focused
+        <div ref={dropdownRef} className="dropdown-wrapper">
+          <input
+            className="dropdown-input"
+            name="dropdown-input"
+            autoFocus
+            onChange={e => setDropdownSearchValue(e.target.value)}
+            value={dropdownSearchValue}
+          />
+          <div className="dropdown-list">
+            <ul>
+              {filteredSkills.map(skill => {
+                return (
+                  <li key={skill} onClick={() => skillSelectionHandler(skill)}>
+                    {skill}{" "}
+                  </li>
+                )
+              })}
+              {filteredSkills.length === 0 && (
+                <li className="no-result">No results found</li>
+              )}
+            </ul>
+          </div>
+        </div>
+      ) : (
+        <input
+          // Grey out the text when "Select Primary skill" input hint is shown
+          className={`dropdown-search ${
+            !(dropdownSearchValue || selectedSkill) && "default"
+          }`}
+          onFocus={() => setEditMode(true)}
+          // Display the selected skill or "Select Primary skill" input hint
+          value={selectedSkill || "Select Primary Geographic Focus"}
+        />
+      )}
+    </div>
       <div className="navy georgia ma0 grow">
         <h2 className="f2">Search for an Expert!</h2>
       </div>
@@ -59,8 +143,15 @@ function Search({ details }) {
         />
       </div>
       {searchList()}
+      
     </section>
+    
   );
 }
 
 export default Search;
+
+
+
+
+
