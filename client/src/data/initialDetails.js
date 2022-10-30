0cfe322768f93105f316332300ded365ff88f273
omar <jlopezal@hamilton.edu>
2022-10-30 19:14:15 -0400

    combining changes

--- client/src/data/initialDetails.js
+++ client/src/data/initialDetails.js
@@ -1,46 +1,39 @@
 const initialDetails = [
-  {
-    id: 1,
-    imgPath: "/assets/Doodle Avatar-01.png",
-    name: "Jane Doe",
-    email: "janedoe@gmail.com",
-    address: "New Delhi, India",
-    interest: "Tennis",
-    
-  },
-  {
-    id: 2,
-    imgPath: "/assets/Doodle Avatar-02.png",
-    name: "Mary Rosamund",
-    email: "agra@rosie.com",
-    address: "Tbilisi, India",
-    interest: "Soccer",
-  },
-  {
-    id: 3,
-    imgPath: "/assets/Doodle Avatar-03.png",
-    name: "Sherlock Watson",
-    email: "irene@johnlock.com",
-    address: "Baker Street, India",
-    interest: "Basketball",
-  },
-  {
-    id: 4,
-    imgPath: "/assets/Doodle Avatar-04.png",
-    name: "John Holmes",
-    email: "mary@johnlock.com",
-    address: "Baker Street, India",
-    interest: "Football",
-  },
-  {
-    id: 5,
-    imgPath: "/assets/Doodle Avatar-05.png",
-    name: "Mycroft Lestrade",
-    email: "britishgovt@gmail.com",
-    address: "London, India",
-    interest: "Squash",
-  },
-];
-
-
-export default initialDetails;
+    {
+      id: 1,
+      imgPath: "/assets/img/1.png",
+      name: "Jane Doe",
+      email: "janedoe@gmail.com",
+      address: "New Delhi, India",
+    },
+    {
+      id: 2,
+      imgPath: "/assets/img/2.png",
+      name: "Mary Rosamund",
+      email: "agra@rosie.com",
+      address: "Tbilisi, India",
+    },
+    {
+      id: 3,
+      imgPath: "/assets/img/3.png",
+      name: "Sherlock Watson",
+      email: "irene@johnlock.com",
+      address: "Baker Street, India",
+    },
+    {
+      id: 4,
+      imgPath: "/assets/img/4.png",
+      name: "John Holmes",
+      email: "mary@johnlock.com",
+      address: "Baker Street, India",
+    },
+    {
+      id: 5,
+      imgPath: "/assets/img/5.png",
+      name: "Mycroft Lestrade",
+      email: "britishgovt@gmail.com",
+      address: "London, India",
+    },
+  ];
+  
+  export default initialDetails;
